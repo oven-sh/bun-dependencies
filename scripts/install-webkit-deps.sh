76a15f0f0adb0aff452b2e87ab0fc91f8916e0d8
sno2 <cartersnook04@gmail.com>
2022-07-14 15:03:14 -0500

    fix commented packages in install webkit deps file

--- scripts/install-webkit-deps.sh
+++ scripts/install-webkit-deps.sh
@@ -1,10 +1,19 @@
 apt-get update
 
+# These are the packages we skip in installation because they're already
+# installed.
+#
+# clang-13 \
+# libc++-13-dev \
+# libc++abi-13-dev \
+# libclang-13-dev \
+# liblld-13-dev \
+# lld-13 \
+
 apt-get install --no-install-recommends -y \
     bc \
     build-essential \
     ca-certificates \
-    # clang-13 \
     clang-format-13 \
     cmake \
     cpio \
@@ -15,12 +24,7 @@ apt-get install --no-install-recommends -y \
     git \
     gnupg2 \
     libicu66 \
-    # libc++-13-dev \
-    # libc++abi-13-dev \
-    # libclang-13-dev \
-    # liblld-13-dev \
     libssl-dev \
-    # lld-13 \
     make \
     ninja-build \
     perl \
