852b05d25ecf645317161855c059af24f79ded75
sno2 <cartersnook04@gmail.com>
2022-07-14 16:43:32 -0500

    hopefully fix webkit build

--- scripts/build-webkit-linux.sh
+++ scripts/build-webkit-linux.sh
@@ -1,5 +1,19 @@
 # TODO(sno2): add error checks after edgy commands
 
+if [ "$WEBKIT_DIR" == "" ] ; then
+	export WEBKIT_DIR=$(pwd)/deps/WebKit
+fi
+
+if ["$CC" == ""] ; then
+	export CC=$(which clang-13)
+fi
+
+if ["$CXX" == ""] ; then
+	export CXX=$(which clang++-13)
+fi
+
+printf $WEBKIT_DIR, "\n"
+
 cd $WEBKIT_DIR
 
 cmake \
@@ -14,7 +28,16 @@ cmake \
 	-DCMAKE_C_COMPILER=$CC \
 	$WEBKIT_DIR
 
-CFLAGS="$CFLAGS -ffat-lto-objects"
-CXXFLAGS="$CXXFLAGS -ffat-lto-objects"
+if [ $? -ne 0 ] ; then
+	printf "Failed to build JSC.\n"
+	exit 1
+fi
 
-cmake --build $WEBKIT_DIR --config $WEBKIT_RELEASE_TYPE -- "jsc" -j$(nproc)
+CFLAGS="$CFLAGS -ffat-lto-objects" && \
+CXXFLAGS="$CXXFLAGS -ffat-lto-objects" && \
+	cmake --build $WEBKIT_DIR --config $WEBKIT_RELEASE_TYPE -- "jsc" -j$(nproc)
+
+if [ $? -ne 0 ] ; then
+	printf "Failed to build WebKit.\n"
+	exit 1
+fi
