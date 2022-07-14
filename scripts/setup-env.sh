74465bcae9f953b87f0f624e5576de2e0d248223
sno2 <cartersnook04@gmail.com>
2022-07-14 18:43:53 -0500

    maybe fix script for building other deps

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,20 +1,21 @@
 set_env () {
-	if [ "$GITHUB_ACTIONS" == true ] ; then
+	if [ "$GH_ACTIONS" == true ] ; then
 		echo "$1=$2" >> $GITHUB_ENV
 	else
 		export $1=$2
 	fi
 }
 
-export OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
+set_env OS_NAME $(uname -s | tr '[:upper:]' '[:lower:]')
 
 if [ "$OS_NAME" == "linux" ] ; then
-	set_env "BREW_DEPS_DIR" "/home/linuxbrew/.linuxbrew/Cellar"
+	set_env BREW_DEPS_DIR "/home/linuxbrew/.linuxbrew/Cellar"
 else
-	set_env "BREW_DEPS_DIR" "/usr/local/Cellar"
+	set_env BREW_DEPS_DIR "/usr/local/Cellar"
 fi
 
 set_env WEBKIT_DIR $(pwd)/deps/WebKit
 set_env WEBKIT_RELEASE_TYPE release
+
 set_env CC "$BREW_DEPS_DIR/llvm@13/13.0.1/bin/clang"
 set_env CXX "$BREW_DEPS_DIR/llvm@13/13.0.1/bin/clang++"
